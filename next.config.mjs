4e7a6f6672e2f0a3ee7a697fe992141d638f93b3
AaryaBhatt9 <bhattaarya192@gmail.com>
2025-07-23 16:36:15 -0700

    new changes in out/docs folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,26 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export',
+//     // basePath: '/portfolio1',
+//     // assetPrefix: '/portfolio1/', 
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = '/portfolio1';
+
 const nextConfig = {
-    output: 'export',
+  output: 'export',
+  basePath: isProd ? repoName : '',
+  assetPrefix: isProd ? repoName + '/' : '',
+  
+  images: {
+    unoptimized: true,  // DISABLE image optimization for static export
+  },
 };
 
 export default nextConfig;
-
-
